2d90ca0c6c334aac71fa125b099208c9196c5474
Pavel Vasin <pavel@vasin.nl>
2015-10-05 17:34:04 +0300

    bump protocol version

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 60014
+#define REQUIRE_VERSION 60016
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 319000;
+    return testnet ? 0 : 872400;
 }
 
 std::string static inline ToString(const CService &ip) {
