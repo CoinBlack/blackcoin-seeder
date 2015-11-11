9da8b9c672fa3f6108ba03ef66d97e26a39c8658
Pavel Vasin <pavel@vasin.nl>
2015-11-11 09:32:04 +0300

    bump protocol version

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 872400;
+    return testnet ? 0 : 900000;
 }
 
 std::string static inline ToString(const CService &ip) {
