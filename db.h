340c9ba88749856baa66c26dee9667cf9316364b
Michel van Kessel <michelvankessel@protonmail.com>
2021-01-13 12:39:50 +0100

    at least block 3,360,000

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 650000 : 3280000;
+    return testnet ? 650000 : 3360000;
 }
 
 std::string static inline ToString(const CService &ip) {
