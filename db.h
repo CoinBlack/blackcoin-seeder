4a01cad50f9d9c821b808459353adcfeb814c8a9
BlackcoinDev <dev@blackcoin.dev>
2023-07-03 11:21:18 +0200

    update allowed height

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 1415000 : 4315000;
+    return testnet ? 1455000 : 4525000;
 }
 
 std::string static inline ToString(const CService &ip) {
